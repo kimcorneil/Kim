b10a9f2edb49d2554105b712ad9f01061611e506
Kim Corneil <kimcorneil@Kims-MacBook-Air.local>
2024-06-07 18:18:39 -0400

    Initail Commit

---
+++ push_test.R
@@ -0,0 +1,27 @@
+#' This is a test script
+#' The tick makes a comment every time
+#' We will run some little functions
+#' Then save this as push_test.R
+#' June 7, 2024
+
+# it would have been more professional to use parameter names 
+myvector <- seq(100, 1, by = -7)
+
+# subest
+myavg <- mean(myvector)
+myavg
+
+# source runs everything
+
+# get the sum of my vector
+
+mysum <- sum(myvector)
+mysum
+
+# standard deviation
+mystd <- sd(myvector)
+mystd
+
+
+
+
