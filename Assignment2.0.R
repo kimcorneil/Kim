e9551c0f99c9aad7e310417a9ede3c0b2abdd8f6
kimcorneil <kcorneil3713@gmail.com>
2024-06-14 13:29:40 -0400

    Figuring out which file to use and how to make changes

--- Assignment2.0.R
+++ Assignment2.0.R
@@ -2,3 +2,6 @@
 #' Kimberly Corneil
 #' June 19, 2024
 #' Coding in R
+#' This is the right assignment I had some struggles starting a file but this is the one to be marked! sorry
+
+#' MY MASTER PLAN
