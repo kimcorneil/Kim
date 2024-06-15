91421973af3ae2da424277f90dc574916e986532
kimcorneil <kcorneil3713@gmail.com>
2024-06-15 13:47:58 -0400

    Adding my master plan

--- Assignment2.0.R
+++ Assignment2.0.R
@@ -5,3 +5,8 @@
 #' This is the right assignment I had some struggles starting a file but this is the one to be marked! sorry
 
 #' MY MASTER PLAN
+#' Prompt the user to give a three digit number using the readline() function
+#' Using the conditional statement function if and else check if the input is numeric
+#' If not ask the user to input a numeric number
+#' If the input is numeric, check if it is a narcissistic number and display the appropriate message
+
